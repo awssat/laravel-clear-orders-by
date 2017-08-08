8384a6cdb9162dd2de4f3f8ffa8338963608f3af
phpfalcon <phpfalcon@gmail.com>
2017-08-08 07:13:51 +0300

    Create clearOrdersByServiceProvider.php

---
+++ src/clearOrdersByServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bader\ClearOrdersBy;
+
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\ServiceProvider;
+
+class clearOrdersByServiceProvider extends ServiceProvider
+{
+    /**
+     * Add a ->clearOrdersBy() to query builder
+     */
+    public function register()
+    {
+        Builder::macro("clearOrdersBy", function () {
+            $this->{$this->unions ? 'unionOrders' : 'orders'} = null;
+
+            return $this;
+        });
+    }
